d36db671df335c66947532ff496c66652a447815
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-08-09 14:45:36 +0530

    Added controller to get all posts and to get the details of a single post.

--- src/controllers/jobs.controller.ts
+++ src/controllers/jobs.controller.ts
@@ -1,12 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../types/error";
-// import createHttpError from "http-errors";
-// return next(createHttpError(404, "Jobs not found"));
+import JobModel from "../model/jobs";
+import createHttpError from "http-errors";
 
-export const getAllJobsController = (_req: Request, res: Response, next: NextFunction) => {
+//get all jobs
+export const getAllJobsController = async(_req: Request, res: Response, next: NextFunction) => {
     try {
+        const jobs = await JobModel.find();
+        return res.status(200).json(jobs);
     } catch (error: AppError | any) {
         console.log(error);
         next({statusCode: 500, message: "Unable to get jobs."});
+    }
+};
+
+//get details of a single job
+export const getJobDetailsController = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const jobID = req.params.id;
+        if(!jobID) return next(createHttpError(400, "Job ID is required."));
+        const jobDetails = await JobModel.findById(jobID);
+        return res.status(200).json({details: jobDetails});
+    } catch (error: AppError | any) {
+        next({statusCode: 500, message: "Unable to get job details."});
     }
 };
--- src/routes/jobs.route.ts
+++ src/routes/jobs.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAllJobsController } from "../controllers/jobs.controller";
+import { getAllJobsController, getJobDetailsController } from "../controllers/jobs.controller";
 
 const jobRouter = Router();
 
 //get all jobs
 jobRouter.get("/", getAllJobsController);
+
+//get details of a single job
+jobRouter.get("/:id", getJobDetailsController);
 
 export default jobRouter;
